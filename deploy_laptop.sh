03b0521372982dead6503f6b5150216750177200
Jackson McCrory <jackson@jacksonmccrory.com>
2020-08-15 21:54:28 +0000

    Added deploy_laptop script

---
+++ deploy_laptop.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Execute base scripts and install programs
+
+cd ~/installscripts/
+sudo ./misc.sh && ./yay.sh && ./nerd-fonts.sh
+sudo pacman --noconfirm -S xcompmgr feh ranger emacs tlp alacritty xmonad xmonad-contrib xmobar ttf-ubuntu-font-family thunderbird
+yay -S brave-bin 
+
+# Moving some files from dotfiles
+
+mkdir -p ~/.config/{alacritty,xmobar}
+
+cd ~
+git clone https://gitlab.com/jadecell/dotfiles
+cd dotfiles
+cp .Xresources ~
+cp .config/xmobar/configs/xmobarrclaptop ~/.config/xmobar/xmobarrc0
+cp .config/alacritty/alacritty.yml ~/.config/alacritty/
+
+# Doom emacs
+
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
+
+# Oh-my-zsh
+ 
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Starting services
+ 
+sudo systemctl enable --now tlp
+
+# Making the .xinitrc
+
+cd ~
+echo "exec xmonad" > ~/.xinitrc
+
+echo ""
+echo "------------FINISHED RICING LAPTOP------------"
+echo ""
+
