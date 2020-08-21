94eb0f146616f88801055491c26aafd7e3390739
Jackson McCrory <jackson@jacksonmccrory.com>
2020-08-21 13:23:56 +0000

    Updated script to handle new file locations

--- deploy_laptop.sh
+++ deploy_laptop.sh
@@ -15,7 +15,7 @@ cd ~
 git clone https://gitlab.com/jadecell/dotfiles
 cd dotfiles
 cp .Xresources ~
-cp .config/xmobar/configs/xmobarrclaptop ~/.config/xmobar/xmobarrc0
+cp .config/xmobarrclaptop/xmobarrclaptop ~/.config/xmobar/xmobarrc0
 cp .config/alacritty/alacritty.yml ~/.config/alacritty/
 cp -r .xmonad ~
 cd ~
