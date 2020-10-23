4f1c33712f64138b956aa1dbb4842f22512dc3a7
Jackson McCrory <jackson@jacksonmccrory.com>
2020-10-22 19:24:19 -0700

    some changes

--- applications/doomemacs.sh
+++ applications/doomemacs.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 # Install dotfiles if not already installed
-! [ -d "$HOME/dotfiles/" ] && git clone https://gitlab.com/jadecell/dotfiles.git
+! [ -d "$HOME/dotfiles/" ] && git clone https://gitlab.com/jadecell/dotfiles.git $HOME/dotfiles/
+sudo pacman --needed --noconfirm -S emacs
 
 # Install DOOM Emacs itself
 git clone --depth 1 https://github.com/hlissner/doom-emacs $HOME/.emacs.d
--- applications/neovim.sh
+++ applications/neovim.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Install dotfiles if not already installed
-! [ -d "$HOME/dotfiles/" ] && git clone https://gitlab.com/jadecell/dotfiles.git
+! [ -d "$HOME/dotfiles/" ] && git clone https://gitlab.com/jadecell/dotfiles.git $HOME/dotfiles/
 
 sudo pacman --needed --noconfirm -S neovim python-pynvim nodejs npm yarn
 
