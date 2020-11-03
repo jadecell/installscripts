c128c92bb0ca23fe972e05229cff9b3ffd1b26e7
Jackson McCrory <jackson@mccrory.xyz>
2020-11-03 00:39:39 -0800

    added xorg drivers

--- applications/misc.sh
+++ applications/misc.sh
@@ -7,6 +7,9 @@
 if [[ "$DISTRO" = "arch" ]]; then
     sudo pacman --noconfirm --needed -S xorg xorg-xinit dmenu termite
 else
+    sudo echo " " >> /etc/portage/make.conf
+    sudo echo "VIDEO_CARDS=\"nouveau qxl intel i915 i965\"" >> /etc/portage/make.conf
+    sudo echo "INPUT_DEVICES=\"libinput synaptics\"" >> /etc/portage/make.conf
     sudo emerge --autounmask-continue x11-base/xorg-x11 x11-terms/rxvt-unicode
 fi
 
