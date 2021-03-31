31c5b93e7152d547fb4e2e055c9faa52fef9ac9b
Jackson McCrory <jackson@mccrory.xyz>
2021-03-30 22:44:32 -0700

    removed sanitize from clang runtime

--- gentoo-packages.sh
+++ gentoo-packages.sh
@@ -27,5 +27,6 @@ sudo flaggie libreoffice -branding
 sudo flaggie sysstat +lm-sensors
 sudo flaggie nerd-fonts +hack +firacode
 sudo flaggie emacs +gui +tiff +png +xwidgets
+sudo flaggie clang-runtime -sanitize
 
 sudo emerge --autounmask-continue x11-base/xorg-x11 media-fonts/joypixels www-client/firefox app-shells/fzf x11-misc/xwallpaper media-gfx/sxiv sys-apps/lsd net-libs/nodejs lxde-base/lxappearance x11-misc/xclip app-text/zathura app-text/zathura-pdf-poppler media-video/mpv x11-misc/dunst media-sound/pulseaudio media-sound/pamixer media-sound/pavucontrol media-sound/playerctl app-misc/ranger dev-libs/libvterm mail-client/thunderbird lxde-base/lxsession app-arch/unzip app-arch/zip app-office/libreoffice app-misc/jq sys-power/acpi media-fonts/nerd-fonts sys-devel/bc x11-misc/trayer x11-misc/xcompmgr app-admin/sysstat media-gfx/scrot media-fonts/cantarell app-editors/emacs sys-apps/bat sys-apps/lm-sensors sys-apps/ripgrep sys-apps/fd x11-misc/xdo media-fonts/ubuntu-font-family net-misc/nextcloud-client
